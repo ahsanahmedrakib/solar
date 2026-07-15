68189f7ccc36828869e8142fb4e7fe9e681bde3b
ahsan.rakib <ahsan.rakib@technonext.com>
2026-07-15 23:12:43 +0600

    HEader and Footer: Information updated

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -60,19 +60,19 @@ export const DEFAULT_SECTIONS: Section[] = [
       {
         label: "Support Email Address",
         type: "email",
-        value: "support@sunexsolar.com",
+        value: "manik@aheadsolarbd.com",
         id: "contact-email",
       },
       {
         label: "Primary Phone Number",
         type: "tel",
-        value: "+1 (800) 555-SOLAR",
+        value: "+8801712947551",
         id: "phone-number",
       },
       {
         label: "Headquarters Address",
         type: "text",
-        value: "100 Renewable Way, Austin, TX 78701",
+        value: "House 12, Road 7, Sector 11, Uttara, Dhaka 1230",
         id: "hq-address",
       },
     ],
@@ -86,7 +86,7 @@ export const DEFAULT_SECTIONS: Section[] = [
       {
         label: "WhatsApp Phone Number (with Country Code)",
         type: "text",
-        value: "+18005557652",
+        value: "+8801712947551",
         id: "whatsapp-number",
       },
       {
@@ -99,7 +99,7 @@ export const DEFAULT_SECTIONS: Section[] = [
       {
         label: "Facebook Messenger Username / Page ID",
         type: "text",
-        value: "sunexsolar",
+        value: "61591154285690",
         id: "messenger-username",
       },
     ],
@@ -153,7 +153,7 @@ export const DEFAULT_SECTIONS: Section[] = [
       {
         label: "Facebook Page URL",
         type: "url",
-        value: "https://facebook.com/sunexsolar",
+        value: "https://www.facebook.com/profile.php?id=61591154285690",
         id: "social-fb",
       },
       {
@@ -178,7 +178,7 @@ export const DEFAULT_SECTIONS: Section[] = [
         label: "Google Map Embed URL",
         type: "url",
         value:
-          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.25280821213!2d-74.11976373059876!3d40.69767006346294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1710000000000!5m2!1sen!2sus",
+          " https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3393.9947761658013!2d90.39066177501897!3d23.87729687858403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c5003133f351%3A0xc57cc5d4675738ff!2sAhead%20Center!5e1!3m2!1sen!2sbd!4v1784135178921!5m2!1sen!2sbd",
         id: "google-map",
       },
     ],
